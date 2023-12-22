2ee45ced673a12104272c01955dd6b957b9a8dab
NikolayOkhlopovskiy <154047449+NikolayOkhlopovskiy@users.noreply.github.com>
2023-12-22 08:02:15 +0200

    Add files via upload
    12 практикум

---
+++ Program-1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompPract12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача #1
+            //Random x = new Random();
+            //int num = x.Next(1, 7);
+            //Console.WriteLine($"Ваше случайное число: {num}");
+            //switch (num)
+            //{
+            //    case 1: Console.WriteLine("Понедельник");
+            //        break;
+            //    case 2: Console.WriteLine("Вторник");
+            //        break;
+            //    case 3: Console.WriteLine("Среда");
+            //        break;
+            //    case 4: Console.WriteLine("Четверг");
+            //        break;
+            //    case 5: Console.WriteLine("Пятница");
+            //        break;
+            //    case 6: Console.WriteLine("Суббота");
+            //        break;
+            //    case 7: Console.WriteLine("Воскресенье");
+            //        break;
+            //}
+
+            // Задача #2
+            //Console.Write("Введите первое целое число: ");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("Введите второе целое число: ");
+            //int y = int.Parse(Console.ReadLine());
+
+            //bool x_chet = x % 2 == 0;
+            //bool y_chet = y % 2 == 0;
+
+            //if (x_chet && y_chet)
+            //{
+            //   x /= 2;
+            //   y /= 2;
+            //}
+            //else if (x_chet || y_chet)
+            //{
+            //    x *= 2;
+            //    y *= 2;
+            //}
+            //else
+            //{
+            //    x++;
+            //    y++;
+            //}
+
+            //Console.WriteLine($"Результат: {x}, {y}");
+
+            // Задача #3 
+            //Console.Write("Введите количество баллов: ");
+            //int score = int.Parse(Console.ReadLine());
+
+            //if (score >= 90 && score <= 100)
+            //{
+            //    Console.WriteLine("Отлично");
+            //}
+            //else if (score >= 70 && score <= 89)
+            //{
+            //    Console.WriteLine("Хорошо");
+            //}
+            //else if (score >= 50 && score <= 69)
+            //{
+            //    Console.WriteLine("Удовлетворительно");
+            //}
+            //else if (score < 50)
+            //{
+            //    Console.WriteLine("Неудовлетворительно");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Некорректное количество баллов");
+            //}
+            Console.Read();
+        }
+    }
+}
